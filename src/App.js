7f92f77b77a76f9b7d8973a46984ed96e6928732
mahana <mahana.delacour@gmail.com>
2018-08-10 11:47:15 +0200

    responsive

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ import logo from './img/TL_Logo_White.png';
 class App extends Component {
   state = {
     cities: [],
-    query: '',
-    results: [],
-    queryArrival: '',
-    resultsArrival: [],
+    query: '',                        // query Search for the Departure
+    results: [],                      // results for the Departure query
+    queryArrival: '',                 // query Search for the Arrival
+    resultsArrival: [],               // results for the Top 5 Arrival query
     enabledList: false,               // for list of Popular Destination
     uniqueName: '',                   // for Get Query Arrival Suggestions
     titleChanged: false               // for Title on Right Panel List
@@ -34,12 +34,10 @@ class App extends Component {
       .catch(error => this.setState({error}));
   };
 
-  getQuery() {
+  getQueryDest() {
     axios.get(`http://www-uat.tictactrip.eu/api/cities/autocomplete/?q=${this.state.query}`)
     // .then(response => console.log(response))
     .then(({data}) => {
-      // let fiveFirst = data;
-      // console.log('data = ', fiveFirst);
       this.setState({
         results: data.slice(0, 6)
       })
@@ -59,7 +57,7 @@ class App extends Component {
 
 
   // List of popular cities On Click
-  handleClick = (e) => {
+  handleClick = (e, local_name) => {
     e.preventDefault();
     this.setState({enabledList: true});
     // console.log('clicked')
@@ -67,20 +65,27 @@ class App extends Component {
     };
 
 
-
   handleClickBtn = (name, uniqueName, isDeparture) => {
     // isDeparture = props.departure (in Suggestions Child )
+
+    // 1° Location in the list has been clicked
       if(isDeparture === true ) {
+    // 2° Change input form to the value of the Location from the list
         this.search.value = name;  // ref of input
         this.setState({
+    // 3° change the unique_name of the API URL to start query of Top Arrivals
           uniqueName}, () => {
             this.getQueryArrival();
           });
-        this.searchDest.focus();
+    // 4° Change the focus of the input to the Arrival input
+        this.searchArr.focus();
+    // 5° Make the Departure List disappear
         this.setState({results: []});
+    // 6° Change the title of the List
         this.setState({titleChanged: true})
       } else {
-        this.searchDest.value = name;
+    // 7° Fill up form input to the selected Location from list
+        this.searchArr.value = name;
       }
   };
 
@@ -90,7 +95,7 @@ class App extends Component {
       }, () => {
         if (this.state.query && this.state.query.length > 1) {
           if (this.state.query.length % 2 === 0) {
-            this.getQuery()
+            this.getQueryDest()
           }
         }
       })
@@ -98,7 +103,7 @@ class App extends Component {
 
   handleInputChangeDestination = () => {
       this.setState({
-        queryArrival: this.searchDest.value
+        queryArrival: this.searchArr.value
       }, () => {
         if (this.state.queryArrival && this.state.queryArrival.length > 1) {
           if (this.state.queryArrival.length % 2 === 1) {
@@ -108,13 +113,9 @@ class App extends Component {
       })
     };
 
-  // componentDidMount() {
-  //   this.getCities();
-  // };
-
   render() {
 
-    const { cities } = this.state;
+    const { cities, titleChanged, query, results, resultsArrival, enabledList } = this.state;
 
     return (
       <div className="app">
@@ -134,6 +135,12 @@ class App extends Component {
           <div className="app_body">
             <h1 className="home_title">European trains and buses at your fingertips.</h1>
             <h2 className="home_subtitle">Travel with Trainline, the independent leader in train and bus ticket sales throughout Europe.</h2>
+            <span className="mobileText">Trainline works even better when you use the app.</span>
+            <div className="mobileBadges">
+              <img src="https://assets.trainline.eu/assets/images/open-home/en/google_play-3380d3b732c0b49bde83314d15ee2c7b.svg" alt="Google Play Badge"/>
+              <img src="https://assets.trainline.eu/assets/images/open-home/en/app_store-8bab50976228b57b171b916a98ddfd11.svg" alt="App Store Badge"/>
+            </div>
+            <span className="mobileTextLink">Or visit our mobile site.</span>
           <div className="search_content">
 
 
@@ -147,15 +154,12 @@ class App extends Component {
                       ref={input => this.search = input}
                       onChange={this.handleInputChange}
                       onClick={(e) => this.handleClick(e)}
-
                     />
                     <input
                       className="input_field input_out"
                       placeholder="Enter your arrival station"
-                      ref={input => this.searchDest = input}
+                      ref={input => this.searchArr = input}
                       onChange={this.handleInputChangeDestination}
-                      // onClick={(uniqueName) => this.handleClickArr(uniqueName)}
-
                     />
 
 
@@ -184,56 +188,47 @@ class App extends Component {
             </div>
             <div className="search_form_right">
               <div>
-
                 <div>
                   <div className="form_title">
                     {
-                      this.state.titleChanged ? 'Select a Popular Destination' : 'Select a departure station'
+                      titleChanged ? 'Select a Popular Destination' : 'Select a departure station'
                     }
-
                   </div>
-                  { this.state.query ?
+                  { query ?
                     <ul className="search_list">
                       <Suggestions
                         departure={true}
-                        results={this.state.results}
+                        results={results}
                         handleClickBtn={this.handleClickBtn}
                       />
                     </ul>
                     : <div>
-                      {this.state.enabledList &&
-                        <ul className="search_list">
-                        {cities.map(city => {
-                          const { local_name } = city;
-                          return (
-                              <li className="search_list_item" key={local_name}>
-                                <img
-                                  className="search_item_icone"
-                                  src="https://assets.trainline.eu/assets/images/location-5632039ea0e607c803bc503fba864f35.svg"
-                                  alt="logo"/>
-                                {local_name}
-                              </li>
-                            );
-                          })}
-                        </ul>
-                      }
-                    </div>
-                  }
+                        {enabledList &&
+                          <ul className="search_list">
+                          {cities.map(city => {
+                            const { local_name } = city;
+                            return (
+                                <li className="search_list_item" key={local_name}>
+                                  <img
+                                    className="search_item_icone"
+                                    src="https://assets.trainline.eu/assets/images/location-5632039ea0e607c803bc503fba864f35.svg"
+                                    alt="logo"/>
+                                  {local_name}
+                                </li>
+                              );
+                            })}
+                          </ul>
+                        }
+                      </div>
+                    }
                 </div>
-
-              {/* when query is inserted and i handleclick input arrival ==> then top5 destinations suggesions should appear */}
-
                 <div>
                   <Suggestions
                     departure={false}
-                    results={this.state.resultsArrival}
-                    handleClickBtn={this.handleClickBtn}
-                  />
+                    results={resultsArrival}
+                    handleClickBtn={this.handleClickBtn} />
                 </div>
-
-
                 <button className="search_list_btn search_item_icon">VIA</button>
-
               </div>
             </div>
           </div>
